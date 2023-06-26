46267a85e69abbc29dd0a06d20a4e333d56ec150
Ваше имя английскими буквами <fieryswv@yandex.ru>
2023-06-26 19:37:13 +0300

    Init commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите произвольное целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральную степень для этого числа: ");
+int b = int.Parse(Console.ReadLine()!);
+int naturalDegree = a;
+
+for (int i=1; i<b; i++)
+{
+  naturalDegree = naturalDegree * a;
+}
+
+Console.Write($"Натуральная степень числа {a}^{b} = {naturalDegree} | ");
---
+++ Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите произвольное целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+int lengthNum = Length(number);
+
+int Length(int number)
+{
+  int index = 0;
+  while (number > 0)
+  {
+    number /= 10;
+    index++;
+  }
+  return index;
+}
+
+int Sum(int number, int lengthNum)
+{
+  int sum = 0;
+  for (int i = 1; i <= lengthNum; i++)
+  {
+    sum += number % 10;
+    number /= 10;
+  }
+  return sum;
+}
+
+Console.Write($"Сумма цифр в числе {number} равна: {Sum(number,lengthNum)}");
